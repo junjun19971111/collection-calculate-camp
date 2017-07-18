00ea32810258cafba8ccf947bbd8266c4235dfeb
junjun19971111 <1121968314@qq.com>
2017-07-18 03:34:31 -0400

    Update choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,10 @@
 'use strict';
-
-function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+let arry=[];
+function get_reapot_num(colllection) {
+    if(arry.indexOf(colllection)===-1){
+        arry.push(colllection);
+        return colllection;
+    }
 }
-
-module.exports = choose_no_repeat_number;
+let x=[1,1,1,1,2,2,3,3,4].filter(get_reapot_num);
+console.log(x);
